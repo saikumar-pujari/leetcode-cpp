36b29bb6b4386d6eaa73947c1440085075c21324
saikumar <159767386+saikumar-pujari@users.noreply.github.com>
2025-03-19 20:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (60.27%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>frq;
-        int n=nums.size();
-        vector<int>ans;
-        for(int val:nums){
-            frq[val]++;
+          int c1 = 0, c2 = 0, ele1 = -1, ele2 = -1;
+    int n = nums.size();
+
+    // Step 1: Find two potential majority candidates
+    for (int num : nums) {
+        if (num == ele1) {
+            c1++;
+        } else if (num == ele2) {
+            c2++;
+        } else if (c1 == 0) {
+            ele1 = num;
+            c1 = 1;
+        } else if (c2 == 0) {
+            ele2 = num;
+            c2 = 1;
+        } else {
+            c1--;
+            c2--;
         }
-        for(auto&[key,value]:frq){
-            if(value>n/3){
-                ans.push_back(key);
-            }
-        }
-        return ans;
+    }
+
+    // Step 2: Verify candidates
+    int count1 = 0, count2 = 0;
+    
+    for (int val : nums) {
+        if (val == ele1) count1++;
+        else if (val == ele2) count2++;
+    }
+
+    vector<int> ans;
+    if (count1 > n / 3) ans.push_back(ele1);
+    if (count2 > n / 3) ans.push_back(ele2);
+
+    return ans;
     }
 };
